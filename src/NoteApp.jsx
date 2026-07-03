03c3a8f551ed4ec974e7660f4f59e65b18d0e686
Afla Shareen <aflasherin07@email.com>
2026-07-03 12:50:59 +0530

    changed the location of noteimage

--- src/NoteApp.jsx
+++ src/NoteApp.jsx
@@ -2,8 +2,8 @@ import notesImg from "./assets/notes.webp";
 function NoteApp(){
     return (
         <div className="flex flex-row pt-5 px-5 py-5 ">
-        <img src={notesImg} alt="Note" className="w-16 h-16 pb-6"/>
-        <h1 className="text-3xl font-bold text-white">Keep Notes</h1>
+        <img src={notesImg} alt="Note" className="w-16 h-16 pb-2"/>
+        <h1 className="text-3xl pt-2 pl-2 font-bold text-white">Keep Notes</h1>
         </div>
     )
     
